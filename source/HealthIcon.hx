9f09d58a81875b2c58d6d1a5f29bb47edef3eef7
Shygee <cole.w.bowman06@gmail.com>
2022-03-18 12:05:51 -0500

    freeplay bump?????

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -9,6 +9,7 @@ using StringTools;
 class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
+	public var canBounce:Bool = false;
 	private var isOldIcon:Bool = false;
 	private var isPlayer:Bool = false;
 	private var char:String = '';
@@ -28,6 +29,12 @@ class HealthIcon extends FlxSprite
 
 		if (sprTracker != null)
 			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+		
+		if(canBounce) {
+			var mult:Float = FlxMath.lerp(1, scale.x, CoolUtil.boundTo(1 - (elapsed * 9), 0, 1));
+			scale.set(mult, mult);
+			updateHitbox();
+		}
 	}
 
 	public function swapOldIcon() {
@@ -66,6 +73,15 @@ class HealthIcon extends FlxSprite
 		offset.x = iconOffsets[0];
 		offset.y = iconOffsets[1];
 	}
+	
+	public function bounce() {
+		if(canBounce) {
+			var mult:Float = 1.2;
+			scale.set(mult, mult);
+			updateHitbox();
+		}
+	}
+
 
 	public function getCharacter():String {
 		return char;
